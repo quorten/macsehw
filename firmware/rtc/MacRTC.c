6328347304a773bff63344ea1a558cbee15d2d19
Andrew Makousky <mako0042@umn.edu>
2020-09-05 18:04:06 -0500

    Improve MacRTC.c file header.

--- firmware/rtc/MacRTC.c
+++ firmware/rtc/MacRTC.c
@@ -1,7 +1,19 @@
-/* Public Domain Dedication: CC0 1.0 Universal.
+/* Macintosh RTC chip drop-in replacement.
 
-   For more information, please see
-   <http://creativecommons.org/publicdomain/zero/1.0/>
+   Written in 2020 by Andrew Makousky
+
+   Public Domain Dedication:
+
+   To the extent possible under law, the author(s) have dedicated all
+   copyright and related and neighboring rights to this software to
+   the public domain worldwide. This software is distributed without
+   any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication
+   along with this software. If not, see
+   <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+   ----------
 
    Developed with reference to a Reddit posting and Mini vMac source.
 
