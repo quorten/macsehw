6f71120bbbc7fdf204f2f6f6dbc57f5c304dbcc6
Andrew Makousky <mako0042@umn.edu>
2020-10-09 19:22:47 -0500

    Note source photo URL.

--- hardware/packaging/macse30/get_mac_halftone.sh
+++ hardware/packaging/macse30/get_mac_halftone.sh
@@ -9,5 +9,6 @@
 
 # NOTE: Processing from original photo:
 # Crop to: upper left corner 606,687 size 456x524
+# SOURCE PHOTO that was processed: https://web.archive.org/web/20201010002142id_/https://i.ebayimg.com/images/g/RFkAAOSwKaVfJwAd/s-l1600.jpg
 
 curl -L -o mac_halftone.png 'https://drive.google.com/uc?export=download&id=18Nl-1DJkCo8ceyCM3i6Pw9AWOyl8iEVY'
